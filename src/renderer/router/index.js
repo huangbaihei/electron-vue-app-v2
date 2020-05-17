17e83f93fb020f7ae33c1dad0df41db7d9f439fb
黄麒二 <huangqier@hogecloud.com>
2020-05-17 15:34:03 +0800

    增加批量生成快递单模板的功能

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -9,7 +9,7 @@ export default new Router({
       path: '/',
       name: 'landing-page',
       // component: require('@/components/LandingPage').default
-      component: require('@/components/xlsx').default
+      component: require('@/components/index').default
     },
     {
       path: '*',
